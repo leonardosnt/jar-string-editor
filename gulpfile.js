0df13fe0924b5fed245d1f6dd8e6c20fa78ba88e
Leonardo Santos <devleeo@outlook.com>
2017-02-11 13:14:06 -0300

    fix build again

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const gulp = require('gulp');
 const webpack = require('gulp-webpack');
 const rename = require('gulp-rename');
 const uglifyjs = require('gulp-uglifyjs');
+const path = require('path');
+
+const DIST_PATH = path.join(__dirname, 'dist/');
 
 gulp.task('default', () => {
   return gulp.start('build')
@@ -12,8 +15,8 @@ gulp.task('default', () => {
 gulp.task('build', () => {
   return gulp.src('src/*.js')
     .pipe(webpack(require('./webpack.config')))
-    .pipe(gulp.dest(__dirname + './dist/'))
+    .pipe(gulp.dest(DIST_PATH))
     .pipe(rename(path => path.extname = ".min.js"))
     .pipe(uglifyjs())
-    .pipe(gulp.dest(__dirname + './dist/'))
+    .pipe(gulp.dest(DIST_PATH))
 });
