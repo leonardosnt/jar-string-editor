6efce91928db2cc4030171036373011ed3ac8de9
Leonardo Santos <devleeo@outlook.com>
2017-02-12 18:18:54 -0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (C) 2017 leonardosnt
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 import { JavaClassFileWriter, JavaClassFileReader, Opcode, ConstantType, InstructionParser } from 'java-class-tools';
 import JSZip from 'jszip';
 import { TextDecoder, TextEncoder } from 'text-encoding';
